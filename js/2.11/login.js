a05b214cfcf0652ef83b231d9dfd69b9fcb2d6c2
unknown <artmasvic@gmail.com>
2023-12-01 15:50:05 +0300

    hz

--- js/2.11/login.js
+++ js/2.11/login.js
@@ -1,10 +1,15 @@
-let login = prompt('Логин', [])
-if (login == 'admin')
-{pass = prompt ("Введите пароль", [])
-    if (pass == '123'){alert('Здравствуйте')}
-    else if (pass == null || pass == ""){alert('Отменено')}
-    else alert('Неверный пароль')}
-else if (login == null || login == "")
-{alert('Отменено')}
-else 
-{alert("Я вас не знаю")}
+let login = prompt('Логин', []);
+if (login == 'admin') {
+pass = prompt ("Введите пароль", []);
+    if (pass == '123'){
+        alert('Здравствуйте');
+    } else if (pass == null || pass == ""){
+        alert('Отменено');
+    } else {
+        alert('Неверный пароль');
+    }
+} else if (login == null || login == "") {
+alert('Отменено');
+} else {
+alert("Я вас не знаю");
+}
