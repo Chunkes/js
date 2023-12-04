858a3b9230a11453bc38dc59bdadadf41cb78e33
unknown <artmasvic@gmail.com>
2023-12-05 00:54:55 +0300

    1

---
+++ js/4.4/1/obj.js
@@ -0,0 +1,18 @@
+let calculator = {
+read() {
+this.a = +prompt("Введите число a")
+this.b = +prompt("Введите число b")
+    },
+
+sum() {
+return this.a + this.b;
+},
+
+mul() {
+    return this.a* this.b;
+    }
+}
+
+calculator.read();
+alert(calculator.mul())
+alert(calculator.sum())
